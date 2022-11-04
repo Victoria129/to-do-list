9d6035fa5293c7401e70912f1ddb9743b9f7d24a
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2022-11-05 01:11:55 +0300

    implement functionality to display todo list and to delete todo task

--- src/index.js
+++ src/index.js
@@ -4,29 +4,10 @@ import Utility from "./utility.js";
 const inputForm = document.getElementById("to-do-form");
 const input = document.getElementById("to-do-input");
 
+inputForm.addEventListener("submit", (e) => {
+e.preventDefault();
+Utility.addTodoTask(input.value);
+input.value = "";
+})
 
-
-
-const createToDoItemHtml = ({ description }) => {
-  const div = document.createElement('div');
-  div.className = 'to-do-item';
-  div.innerHTML = `
-    <div class="to-do-detail">
-    <input type="checkbox">
-    <h3 class="to-do-dsc">${description}</h3>
-</div>
-<div>
-    <button class="edit-btn"><i class="fa-regular fa-pen-to-square"></i></button>
-    <button class="delete-btn"><i class="fa-solid fa-trash-can"></i></button>
-</div>
-`;
-  return div;
-};
-
-const showToDoItems = () => {
-  toDoList.sort((a, b) => a.index - b.index);
-  toDoList.forEach((item) => {
-    document.querySelector('.to-do-list').append(createToDoItemHtml(item));
-  });
-};
-showToDoItems();
+Utility.showToDoItems();
--- src/utility.js
+++ src/utility.js
@@ -13,6 +13,62 @@ export default class Utility {
         } else {
             todoList = JSON.parse(localStorage.getItem("todoListItems"));
         }
+        return todoList;
+    }
+
+    static deleteItem = (id) => {
+        let todoList = this.getLocalStorageList();
+        let itemToDel = todoList[id];
+        todoList = todoList.filter((item) => item !== itemToDel);
+        this.setLocalStorageList(todoList);
+    }
+
+    static addRemoveEvent = () => {
+        document.querySelectorAll(".delete-btn").forEach((button) => button.addEventListener('click', (e) => {
+            e.preventDefault();
+            let id = button.id - 1;
+            console.log(id);
+            this.deleteItem(id);
+            this.showToDoItems();
+        }))
+    }
+
+    static createToDoItemHtml = ({ description, index }) => {
+        const div = document.createElement('div');
+        div.className = 'to-do-item';
+        div.innerHTML = `
+          <div class="to-do-detail">
+          <input type="checkbox">
+          <h3 class="to-do-dsc">${description}</h3>
+      </div>
+      <div>
+          <button class="edit-btn"><i class="fa-regular fa-pen-to-square"></i></button>
+          <button class="delete-btn" id="${index}"><i class="fa-solid fa-trash-can"></i></button>
+      </div>
+      `;
+        return div;
+      };
+      
+      static showToDoItems = () => {
+        let toDoList = this.getLocalStorageList();
+        document.querySelector('.to-do-list').innerHTML = "";
+        toDoList.sort((a, b) => a.index - b.index);
+        toDoList.forEach((item) => {
+          document.querySelector('.to-do-list').append(this.createToDoItemHtml(item));
+        });
+
+        this.addRemoveEvent();
+      };
+      
+
+    static addTodoTask = (description) => {
+        let todoList = this.getLocalStorageList();
+        let index = todoList.length + 1;
+        let newTask = new Todo(description, index);
+
+        todoList.push(newTask);
+        this.setLocalStorageList(todoList);
+        this.showToDoItems()
     }
 
 }
