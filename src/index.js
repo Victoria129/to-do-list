024aed071609e4d3c0d15f6de0e35a44006e7311
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2022-11-04 17:55:22 +0300

    create js function for generating to do list html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './index.css';
+
+let toDoList = [
+    {
+        description: "Go swimming",
+        completed: false,
+        index: 1
+    },
+    {
+        description: "Visit dentist",
+        completed: false,
+        index: 2
+    },
+    {
+        description: "Plan party",
+        completed: false,
+        index: 3
+    }
+]
+
+const createToDoItemHtml = () => {
+    const div = document.createElement("div");
+    div.className = "to-do-item";
+    div.innerHTML = `
+    <div class="to-do-detail">
+    <input type="checkbox">
+    <h3 class="to-do-dsc">Go swimming</h3>
+</div>
+<div>
+    <button class="edit-btn"><i class="fa-regular fa-pen-to-square"></i></button>
+    <button class="delete-btn"><i class="fa-solid fa-trash-can"></i></button>
+</div>
+`;
+    return div;
+}
+
+
