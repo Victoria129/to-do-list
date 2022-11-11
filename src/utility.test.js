e88d0441e8d58a2bbc5f67266e478d58f8d63d4e
Victoria129 <victoria.tumwebaze@outlook.com>
2022-11-12 00:03:39 +0300

    Fix linter errors

--- src/utility.test.js
+++ src/utility.test.js
@@ -1,25 +1,24 @@
 /** @jest-environment jsdom */
-import Utility from './utility'
+import Utility from './utility';
 
 describe('Add new todo task', () => {
-
-beforeEach(() => {
-  localStorage.clear();
-  jest.clearAllMocks();
-  localStorage.setItem.mockClear();
-  document.body.innerHTML = `
+  beforeEach(() => {
+    localStorage.clear();
+    jest.clearAllMocks();
+    localStorage.setItem.mockClear();
+    document.body.innerHTML = `
   <div class="to-do-list">
   </div>
-`
-})
+`;
+  });
 
-  test('Add new object to local storage', () =>{
+  test('Add new object to local storage', () => {
     // Arrange
     const obj = {
       description: 'new todo item',
       index: 1,
       completed: false,
-    }
+    };
 
     // Act
     const addTodoTaskSpy = jest.spyOn(Utility, 'addTodoTask');
@@ -27,14 +26,14 @@ beforeEach(() => {
 
     // Assert
     expect(addTodoTaskSpy).toHaveBeenCalledTimes(1);
-    const result = JSON.parse(window.localStorage.getItem('todoListItems'))
+    const result = JSON.parse(window.localStorage.getItem('todoListItems'));
     expect(result).toHaveLength(1);
-  })
+  });
 
-  test('Add two new objects to local storage', () =>{
+  test('Add two new objects to local storage', () => {
     // Arrange
-    const dscOne = 'Going to work'
-    const dscTwo = 'Find lunch'
+    const dscOne = 'Going to work';
+    const dscTwo = 'Find lunch';
 
     // Act
     const addTodoTaskSpy = jest.spyOn(Utility, 'addTodoTask');
@@ -43,14 +42,14 @@ beforeEach(() => {
 
     // Assert
     expect(addTodoTaskSpy).toHaveBeenCalledTimes(2);
-    const result = JSON.parse(window.localStorage.getItem('todoListItems'))
+    const result = JSON.parse(window.localStorage.getItem('todoListItems'));
     expect(result).toHaveLength(2);
     expect(result[0].description).toEqual(dscOne);
-  })
+  });
 
-  test('Add new task to UI', () =>{
+  test('Add new task to UI', () => {
     // Arrange
-    const dscOne = 'Going to work'
+    const dscOne = 'Going to work';
 
     // Act
     const addTodoTaskSpy = jest.spyOn(Utility, 'addTodoTask');
@@ -58,13 +57,12 @@ beforeEach(() => {
 
     // Assert
     expect(addTodoTaskSpy).toHaveBeenCalledTimes(1);
-    const todoDiv = document.querySelector('.to-do-list')
-    expect(todoDiv.innerHTML).toBeDefined()
-  })
-})
+    const todoDiv = document.querySelector('.to-do-list');
+    expect(todoDiv.innerHTML).toBeDefined();
+  });
+});
 
 describe('Delete todo task', () => {
-
   beforeEach(() => {
     localStorage.clear();
     jest.clearAllMocks();
@@ -72,43 +70,39 @@ describe('Delete todo task', () => {
     document.body.innerHTML = `
     <div class="to-do-list">
     </div>
-  `
-  })
-  
-  test('Item to be deleted from local storage', () => {
+  `;
+  });
 
+  test('Item to be deleted from local storage', () => {
     const obj = {
       description: 'new todo item',
       index: 1,
       completed: false,
-    }
+    };
     window.localStorage.setItem('todoListItems', JSON.stringify([obj]));
     const id = 0;
 
     // Act
     const deleteTodoTaskSpy = jest.spyOn(Utility, 'deleteItem');
     Utility.deleteItem(id);
-    const result = JSON.parse(window.localStorage.getItem('todoListItems'))
-   
+    const result = JSON.parse(window.localStorage.getItem('todoListItems'));
 
     expect(deleteTodoTaskSpy).toHaveBeenCalledTimes(1);
     expect(result).toHaveLength(0);
-  })
+  });
 
   test('Check if the correct item to be deleted', () => {
-
     const objOne = {
       description: 'new todo item',
       index: 1,
       completed: false,
-    }
+    };
 
     const objTwo = {
       description: 'Finish your work',
       index: 2,
       completed: false,
-    }
-
+    };
 
     window.localStorage.setItem('todoListItems', JSON.stringify([objOne, objTwo]));
     const id = 0;
@@ -116,11 +110,10 @@ describe('Delete todo task', () => {
     // Act
     const deleteTodoTaskSpy = jest.spyOn(Utility, 'deleteItem');
     Utility.deleteItem(id);
-    const result = JSON.parse(window.localStorage.getItem('todoListItems'))
-   
+    const result = JSON.parse(window.localStorage.getItem('todoListItems'));
 
     expect(deleteTodoTaskSpy).toHaveBeenCalledTimes(1);
-    expect(result[0].index).toBe(1)
+    expect(result[0].index).toBe(1);
     expect(result).toHaveLength(1);
-  })
-})
+  });
+});
